4432b8f1d458ee4baba798442a0691e144db2791
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2023-12-31 09:44:00 -0500

    added js for select options

--- src/script.js
+++ src/script.js
@@ -21,3 +21,28 @@ function showCityTime() {
 }
 showCityTime();
 setInterval(showCityTime, 1000);
+
+function updateCity(event) {
+  setInterval(() => {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.split("/")[1].replace("Vancouver", "Seattle");
+
+    let cityTime = moment().tz(cityTimeZone).format("hh:mm:ss");
+    let cityDate = moment().tz(cityTimeZone).format("MMMM Do YYYY");
+
+    let cityUpdateElement = document.querySelector("#city-update");
+    cityUpdateElement.innerHTML = `<div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityDate}</div>
+  </div>
+  <div class="time">${cityTime}<small> ${moment()
+      .tz(cityTimeZone)
+      .format("A")}</small></div>
+    </div>
+`;
+  }, 1000);
+}
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
